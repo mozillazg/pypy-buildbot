05b72d11dab3c0483dca41a4a79ba8ce29ebaa38
Matti Picus <matti.picus@gmail.com>
2021-10-10 14:32:44 +0300

    use --no-check-certificate for centos6 wget, remove this for centos7

--- docker/install_xz5.sh
+++ docker/install_xz5.sh
@@ -2,7 +2,9 @@
 set -xe pipefail
 XZ5_FILE=xz-5.2.4.tar.gz
 
-wget -q "https://tukaani.org/xz/${XZ5_FILE}"
+# Let's encrypt's certificate expired Sept 2021, it is difficult to update on
+# centos6. When we move to centos7, remove the --no-check-certificate
+wget --no-check-certificate -q "https://tukaani.org/xz/${XZ5_FILE}"
 gpg --import lasse_collin_pubkey.txt
 gpg --verify ${XZ5_FILE}.sig ${XZ5_FILE}
 
