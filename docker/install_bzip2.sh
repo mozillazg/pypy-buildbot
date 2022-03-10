51bf7a62edbc353ddc8b54e96fc8dde523700a9e
Matti Picus <matti.picus@gmail.com>
2022-03-10 16:19:24 +0200

    fix bzip2 install script to install *.so

--- docker/install_bzip2.sh
+++ docker/install_bzip2.sh
@@ -3,24 +3,28 @@ set -xeo pipefail
 
 BZIP2_VERSION="1.0.8"
 
-function check_sha256sum {
+function check_sha512sum {
     local fname=$1
-    local sha256=$2
-    echo "${sha256}  ${fname}" > "${fname}.sha256"
-    sha256sum -c "${fname}.sha256"
-    rm "${fname}.sha256"
+    local sha512=$2
+    echo "${sha512}  ${fname}" > "${fname}.sha512"
+    sha512sum -c "${fname}.sha512"
+    rm "${fname}.sha512"
 }
 
-wget https://sourceware.org/pub/bzip2/bzip2-${BZIP2_VERSION}.tar.gz
+wget --no-check-certificate https://sourceware.org/pub/bzip2/bzip2-${BZIP2_VERSION}.tar.gz
+SHA512="083f5e675d73f3233c7930ebe20425a533feedeaaa9d8cc86831312a6581cefbe6ed0d08d2fa89be81082f2a5abdabca8b3c080bf97218a1bd59dc118a30b9f3"
+
 tar zxf bzip2*.tar.gz
+rm bzip2*.tar.gz*
 pushd bzip2*
-CONFIGURE_PRE="--prefix=/usr/local --enable-shared=yes --enable-static=yes --disable-dependency-tracking"
 CFLAGS="-fPIC ${CFLAGS}"
 if [ "$1" == "m32" ]; then
-  setarch i386 ./configure ${CONFIGURE_PRE} CFLAGS="-m32 ${CFLAGS}"
+  CFLAGS="-m32 ${CFLAGS}"
+  setarch i386 make -f Makefile-libbz2_so
 else
-  ./configure ${CONFIGURE_PRE} CFLAGS=${CFLAGS}
+  make -f Makefile-libbz2_so
 fi
 make install
+cp -r libbz2.so* /usr/local/lib
 popd
 rm -rf bzip2*
