585ad55a20fa57e0d80b59e0cdf87995a0486fd5
Matti Picus <matti.picus@gmail.com>
2022-12-11 16:58:06 -0500

    update to xz-5.2.5

--- docker/install_xz5.sh
+++ docker/install_xz5.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -xe pipefail
-XZ5_FILE=xz-5.2.4.tar.gz
+XZ5_FILE=xz-5.2.5.tar.gz
 
-# Let's encrypt's certificate expired Sept 2021, it is difficult to update on
-# centos6. When we move to centos7, remove the --no-check-certificate
-wget --no-check-certificate -q "https://tukaani.org/xz/${XZ5_FILE}"
+wget -q "https://tukaani.org/xz/${XZ5_FILE}"
 gpg --import lasse_collin_pubkey.txt
 gpg --verify ${XZ5_FILE}.sig ${XZ5_FILE}
 
